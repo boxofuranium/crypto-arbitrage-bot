494acbbec7f2b17837cfbda7215b88b955993a2d
Akay <akaay1@icloud.com>
2023-12-14 22:17:16 -0800

    exchange_api.py

--- src/exchange_api.py
+++ src/exchange_api.py
@@ -1,5 +1,5 @@
 import requests
-import json  # Add this line to import the json module
+import json
 
 class ExchangeAPI:
     def __init__(self, api_key, api_secret, api_passphrase, exchange_name):
@@ -24,22 +24,18 @@ class ExchangeAPI:
 
         response = self.client.get(url)
 
-        # Handle JSONP callback
+        
         if callback:
             json_start = response.text.find('(') + 1
             json_end = response.text.rfind(')')
             json_content = response.text[json_start:json_end]
 
             try:
-                # Parse the extracted JSON content
+                
                 return json.loads(json_content)
             except json.JSONDecodeError as json_err:
                 print(f"Error decoding JSON content: {json_err}")
-                return None  # Return None or handle the error as needed
+                return None  
 
         response.raise_for_status()
         return response.json()
-
-# Example usage with JSONP callback
-# exchange_client = ExchangeAPI(api_key='your_api_key', api_secret='your_api_secret', api_passphrase='your_passphrase', exchange_name='your_exchange')
-# ticker_data = exchange_client.get_ticker_data(callback='your_callback_function')
